44315c177c08e1a2c9dc2cea2cea91cd53940e81
seancase14 <seancase14@gmail.com>
2025-08-07 20:09:35 -0400

    added else branch to nested if-else statement, handles all other user input other than 'yes'

--- script.py
+++ script.py
@@ -3,8 +3,10 @@ print("how is it going?\n")
 mood = input()
 
 if mood.lower() == 'good':
-    print("I'm so glad to hear that, would you like me to read you a happy poem?")
+    x = input("I'm so glad to hear that, would you like me to read you a happy poem?")
     if x.lower() == 'yes':
         print("Roses are red, violets are blue, you make life easy, I like you")
+    else:
+        print("Okay, have a great day!\n")
 elif mood.lower() == 'bad':
-    print("I'm so sorru, I can try to cheer you up with a powerful quote if you want?\n")
+    x = input("I'm so sorru, I can try to cheer you up with a powerful quote if you want?\n")
