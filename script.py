319be99682dd783470df0045204e6ff2320cdb26
seancase14 <seancase14@gmail.com>
2025-08-07 20:03:31 -0400

    added nested if statement on line 7 to display poem if user input is 'yes'

--- script.py
+++ script.py
@@ -4,5 +4,7 @@ mood = input()
 
 if mood.lower() == 'good':
     print("I'm so glad to hear that, would you like me to read you a happy poem?")
+    if x.lower() == 'yes':
+        print("Roses are red, violets are blue, you make life easy, I like you")
 elif mood.lower() == 'bad':
     print("I'm so sorru, I can try to cheer you up with a powerful quote if you want?\n")
